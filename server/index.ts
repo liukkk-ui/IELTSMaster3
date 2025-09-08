e60d24a4d17b0b77aaa7808f302cf913d43da5c8
liukkk-ui <summerliu0522@gmail.com>
2025-09-08 20:18:09 +0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -35,9 +35,6 @@ app.get("/test", (req, res) => {
   res.send(`Views: ${req.session.views}`);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
