f87bbd72471c1d70f409d3757c889610d2b36915
liukkk-ui <summerliu0522@gmail.com>
2025-09-09 09:47:28 +0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import session from "express-session";
 import connectPgSimple from "connect-pg-simple";
-import pkg from "pg";
+import pgk from "pg";
 
 const { pool } = pgk;
 const app = express();
